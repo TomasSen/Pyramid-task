7a797704c456bf52523289814934b195faedad69
solidconcrete <48732500+solidconcrete@users.noreply.github.com>
2020-07-02 10:38:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, temp;
+            Console.WriteLine("Waiting for pyramid size... ");
+            size = int.Parse(Console.ReadLine());
+            for (int line = size ; line >= 0; line--)
+            {
+                for (temp = 0; temp < line; temp ++)
+                {
+                    Console.Write(" ");
+                }
+                for (temp = temp; temp < size; temp ++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
